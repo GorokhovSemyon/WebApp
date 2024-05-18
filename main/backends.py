b737247a123f7fcaad307ffa3646a089b5443bb5
Горохов Семён <92113292+GorokhovSemyon@users.noreply.github.com>
2024-05-18 18:03:15 +0300

    Update backends.py

--- main/backends.py
+++ main/backends.py
@@ -2,6 +2,9 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 class EmailBackend(ModelBackend):
+    """
+        Класс авторизации по почте
+    """
     def authenticate(self, request, email=None, password=None, **kwargs):
         user_model = get_user_model()
         try:
